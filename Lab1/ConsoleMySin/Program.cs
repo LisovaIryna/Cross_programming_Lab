eb3865b2af86b6b6f0d634a521d66a4d3e534e73
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2021-02-27 20:15:16 +0200

    49
    Cross_programming_Lab1

---
+++ Lab1/ConsoleMySin/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace ConsoleMySin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+            Console.WriteLine("Task 1:");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" Enter the number: ");
+
+            int number;
+            if (!Int32.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An integer must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            double hundred_count = MyLib.MySin.Hundred_count(number);
+            Console.WriteLine(" Number of hundreds: " + hundred_count);
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+            Console.WriteLine("Task 2:");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" Enter the n: ");
+
+            int n;
+            if (!Int32.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An integer must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            if (n <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" A number greater than zero must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine();
+            Console.Write(" Enter the a: ");
+
+            double a;
+            if (!Double.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An double must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            double multiplication_P = MyLib.MySin.Multiplication_P(n, a);
+            Console.WriteLine(" P: " + multiplication_P);
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+            Console.WriteLine("Task 3:");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write(" Enter the eps: ");
+
+            double eps;
+            if (!Double.TryParse(Console.ReadLine(), out eps))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An integer must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            if (eps <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" A number greater than zero must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Write(" Enter the n: ");
+
+            int n_;
+            if (!Int32.TryParse(Console.ReadLine(), out n_))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An integer must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            if (n_ <= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine(" A number greater than zero must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            Console.Write(" Enter the x: ");
+
+            double x;
+            if (!Double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine();
+                Console.WriteLine(" Incorrect input. An double must be entered.");
+                Console.WriteLine(" Press \"Enter\" and the program will shut down.");
+                Console.WriteLine("────────────────────────────────────────────────────────────");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            double sum_row = MyLib.MySin.Sum_row(eps, n_, x);
+            double function_x = MyLib.MySin.Function_x(eps, n_, x);
+            double сalculation_error = MyLib.MySin.Calculation_error(sum_row,function_x);
+            Console.WriteLine();
+            Console.WriteLine(" Amount: " + sum_row);
+            Console.WriteLine(" Function x: " + function_x);
+            Console.WriteLine(" Calculation error: " + сalculation_error);
+            Console.WriteLine("────────────────────────────────────────────────────────────");
+        }
+    }
+}
---
+++ Lab1/MyLib/MySin.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace MyLib
+{
+    /// Сalculation of mathematical functions
+
+    public class MySin
+    {
+        /// <summary>
+        /// Hundred_count(number)
+        /// </summary>
+        /// <param name="number">an integer entered from the keyboard by the user — argument of the Hundred_count function</param>
+        /// <returns>Returns the value of the Hundred_count function (number of hundreds) for a given integer.</returns>
+        
+        public static int Hundred_count(int number)
+        {
+            if (number < 0)
+            {
+                number = number * -1;
+            }
+
+            Console.WriteLine();
+
+            int hundred_count;
+            hundred_count = number / 100;
+                        
+            return hundred_count;
+        }
+
+        /// <summary>
+        /// Multipliation_P(n,a)
+        /// </summary>
+        /// <param name="n">an integer entered from the keyboard by the user — first argument of the Multiplication_P function</param>
+        /// <param name="a">an double entered from the keyboard by the user — second argument of the Multiplication_P function</param>
+        /// <returns>Returns the value of the Multiplication_P  for a given integer and double.</returns>
+
+        public static double Multiplication_P(int n, double a)
+        {
+            double P = 1;
+            if (n == 1)
+            {
+                P = a;
+            }
+
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    P = P * (a + i - 1);
+                }
+            }
+
+            Console.WriteLine();
+
+            return P;
+        }
+
+        /// <summary>
+        /// Sum_row(eps,n_,x)
+        /// </summary>
+        /// <param name="eps">an double entered from the keyboard by the user — first argument of the Sum_row function</param>
+        /// <param name="n_">an int entered from the keyboard by the user — second argument of the Sum_row function</param>
+        /// <param name="x">an double entered from the keyboard by the user — third argument of the Sum_row function</param>
+        /// <returns>Returns the value of the Sum_row  for a given integer and double.</returns>
+
+        public static double Sum_row(double eps, int n_, double x)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n_; i++)
+            {
+                sum += Math.Pow(-1, 3 * i + 1) * (Math.Pow(x, 2 * i + 1) / (4 * Math.Pow(i, 2) - 1));
+                if (Math.Abs(sum) < eps)
+                {
+                    break;
+                }
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// Function_x(eps,n_,x)
+        /// </summary>
+        /// <param name="eps">an double entered from the keyboard by the user — first argument of the Function_x function</param>
+        /// <param name="n_">an int entered from the keyboard by the user — second argument of the Function_x function</param>
+        /// <param name="x">an double entered from the keyboard by the user — third argument of the Function_x function</param>
+        /// <returns>Returns the value of the Function_x  for a given integer and double.</returns>
+
+        public static double Function_x(double eps, int n_, double x)
+        {
+            double function_x = (1 + Math.Pow(x, 2)) / 2 * Math.Atan(x) - x / 2;
+
+            return function_x;
+        }
+
+        /// <summary>
+        /// Calculation_error(sum,function_x)
+        /// </summary>
+        /// <param name="sum">an double from the Sum_row function — first argument of the Calculation_error function</param>
+        /// <param name="function_x">an double from the Sum_row function — second argument of the Calculation_error function</param>
+        /// <returns>Returns the value of the Calculation_error  for a given double and double.</returns>
+
+        public static double Calculation_error(double sum, double function_x)
+        {
+            double сalculation_error = Math.Abs(sum - function_x) / function_x * 1;
+
+            return сalculation_error;
+        }
+    }
+}
